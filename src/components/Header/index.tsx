26396bcdcaca85c2f844d72c51254e6cb8f8ac06
RogerioMattos <rogeriomattos38@outlook.com>
2021-04-20 14:54:59 -0300

    adicionando assets e component header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './styles.module.scss';
+import format  from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+export function Header() {
+    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+        locale: ptBR
+    });
+
+
+    return(
+        <header className={styles.container}>
+           <img src="assets/logo.svg" alt="Podcastr"/> 
+
+           <p>O melhor para você ovuri, sempre</p>
+
+           <span>{currentDate}</span>
+        </header>
+    );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head'
+import { Header } from '../components/Header';
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Header/>
   )
 }
