c5588a18bdcc005f8d9db22100654c5061de1fca
rogerio.mattos7 <rogeriomattos38@outlook.com>
2021-04-21 11:08:59 -0300

    implementando static page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,25 @@
+import { useEffect } from 'react';
 import { Header } from '../components/Header';
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props.episodes);
+  useEffect(() => {
+    
+  }, []);
+
   return (
    <h1>Home</h1>
   )
 }
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
+};
